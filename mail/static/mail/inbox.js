c364c040cb9094ced9c9debf88f13f94a322d822
deborahly <97473884+deborahly@users.noreply.github.com>
2022-05-06 01:46:41 -0400

    Add email view, read

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -38,6 +38,7 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector('#email-view').style.display = 'none';
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -64,96 +65,117 @@ function load_mailbox(mailbox) {
   document.querySelector('#compose-view').style.display = 'none';
   document.querySelector('#email-view').style.display = 'none';
 
+  // Clear message
+  document.getElementById('message').innerHTML = '';
+
+  // Clear out composition fields
+  document.querySelector('thead').innerHTML = '';
+  document.querySelector('tbody').innerHTML = '';
+
   // Show the mailbox name
   document.querySelector('#title').innerHTML = `${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}`;
-  // document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
-  // Request SENT emails from server
-  if (mailbox == 'sent') {
+  // Request emails from server, depending on the mailbox
+  fetch(`http://127.0.0.1:8000/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(data => {
     
-    fetch('http://127.0.0.1:8000/emails/sent')
-    .then(response => response.json())
-    .then(data => {
-      
-      if (data.length === 0) {
-        document.getElementById('message').innerHTML = "Mailbox empty";
-      }
+    if (data.length === 0) {
+      document.getElementById('message').innerHTML = "Mailbox empty";
+    }
 
-      else {
-        // Create table header
-        const cols_row = document.createElement('tr')
+    else {
+      // Create table header
+      const cols_row = document.createElement('tr')
 
-        const sender_col = document.createElement('th')
-        sender_col.innerHTML = 'From';
-        sender_col.scope = 'col';
+      const sender_col = document.createElement('th')
+      sender_col.innerHTML = 'From';
+      sender_col.scope = 'col';
 
-        const subject_col = document.createElement('th')
-        subject_col.innerHTML = 'Subject';
-        subject_col.scope = 'col';
+      const subject_col = document.createElement('th')
+      subject_col.innerHTML = 'Subject';
+      subject_col.scope = 'col';
 
-        const timestamp_col = document.createElement('th')
-        timestamp_col.innerHTML = 'Date';
-        timestamp_col.scope = 'col';
+      const timestamp_col = document.createElement('th')
+      timestamp_col.innerHTML = 'Date';
+      timestamp_col.scope = 'col';
 
-        cols_row.append(sender_col);
-        cols_row.append(subject_col);
-        cols_row.append(timestamp_col);
+      cols_row.append(sender_col);
+      cols_row.append(subject_col);
+      cols_row.append(timestamp_col);
 
-        document.querySelector('thead').append(cols_row);
+      document.querySelector('thead').append(cols_row);
 
-        // Create table body with emails 
-        data.forEach(function(element, index) {
-          const email = document.createElement('tr');
+      // Create table body with emails 
+      data.forEach(function(element, index) {
+        const email = document.createElement('tr');
 
-          const sender = document.createElement('th');
-          sender.innerHTML = data[index]['sender'];
-          sender.scope = 'col';
-    
-          const subject = document.createElement('th');
-          subject.innerHTML = data[index]['subject'];
-          subject.scope = 'col';
-    
-          const timestamp = document.createElement('th');
-          timestamp.innerHTML = data[index]['timestamp'];
-          timestamp.scope = 'col';
-    
-          email.append(sender);
-          email.append(subject);
-          email.append(timestamp);
-    
-          email.addEventListener('click', function() {
-            console.log('This element has been clicked!')
-          })
-    
-          document.querySelector('tbody').append(email); 
-        })
-      }
-    });
+        const sender = document.createElement('th');
+        sender.innerHTML = data[index]['sender'];
+        sender.scope = 'col';
   
-    return false;
-  }
+        const subject = document.createElement('th');
+        subject.innerHTML = data[index]['subject'];
+        subject.scope = 'col';
+  
+        const timestamp = document.createElement('th');
+        timestamp.innerHTML = data[index]['timestamp'];
+        timestamp.scope = 'col';
+  
+        email.append(sender);
+        email.append(subject);
+        email.append(timestamp);
+
+        if (data[index]['read'] === true) {
+          email.classList.add('gray');
+        }
+  
+        email.addEventListener('click', function() {           
+          // Get email
+          fetch(`http://127.0.0.1:8000/emails/${data[index]['id']}`)
+          .then(response => response.json())
+          .then(data => {
+            document.querySelector('#emails-view').style.display = 'none';
+            document.querySelector('#compose-view').style.display = 'none';
+            document.querySelector('#email-view').style.display = 'block';
+
+            const subject = data['subject'];
+            const sender = data['sender'];
+            const recipients = data['recipients'];
+            const timestamp = data['timestamp'];
+            const body = data['body'];
+          
+            document.querySelector('#subject').innerHTML = `Subject: ${subject}`;
+            document.querySelector('#sender').innerHTML = `From: ${sender}`;
+            document.querySelector('#recipients').innerHTML = `To: ${recipients}`;
+            document.querySelector('#timestamp').innerHTML = timestamp;
+            document.querySelector('#body').innerHTML = body;
+          });
+
+          // Marks as read
+          const json_body = { 
+            read: 'True'
+          }
+
+          const requestOptions = {    
+    
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(json_body)
+          }
+
+          fetch(`http://127.0.0.1:8000/emails/${data[index]['id']}`, requestOptions)
+          .then(response => {
+            if (response.ok) { console.log("HTTP request successful") }
+            else { console.log("HTTP request unsuccessful") }
+            return response
+          });
+        });
+
+        document.querySelector('tbody').append(email); 
+      })
+    }
+  });
+
+  return false;
 }
-
-// async function get_email(email_id) {
-
-//   document.querySelector('#emails-view').style.display = 'none';
-//   document.querySelector('#compose-view').style.display = 'none';
-//   document.querySelector('#email-view').style.display = 'block';
-
-//   fetch('emails/email_id')
-//   .then(response => response.json())
-//   .then(data => {
-
-//     const subject = data['subject'];
-//     const sender = data['sender'];
-//     const timestamp = data['timestamp'];
-//     const body = data['body'];
-
-//     document.querySelector('#subject').innerHTML = subject;
-//     document.querySelector('#sender').innerHTML = sender;
-//     document.querySelector('#timestamp').innerHTML = timestamp;
-//     document.querySelector('#body').innerHTML = body;
-//   })
-
-//   return false;
-// }
